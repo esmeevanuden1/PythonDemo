e7302e420af49dc48cd28590b6fb0f628dbcdf98
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2025-11-07 12:07:07 +0100

    felix 1

---
+++ felix.py
@@ -0,0 +1,33 @@
+print("hoi allemaal")
+a = 15
+
+a = "maandag"
+
+print(a)
+b = input("noem je voornaam: ")
+print("je voornaam is: " +b)
+if b == "joost":
+    print("je bent niet welkom")
+    print("go")
+leeftijd = input("wat is je leeftijd")
+leeftijd = int(leeftijd)
+print(leeftijd + 25)
+print("over 25 jaar badsfasdfen je dus ", leeftijd)
+def ditendat():
+    print("doe dit en dat")
+
+ditendat()
+ditendat()
+ditendat()
+ditendat()
+ditendat()
+ditendat()
+ditendat()
+ditendat()
+ditendat()
+
+def tweedefunctie(param):  # parameter
+    print("print dit maar", param)
+
+tweedefunctie(34) # argument
+tweedefunctie(42)
