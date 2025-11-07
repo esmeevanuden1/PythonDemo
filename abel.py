1e7226480f2be62d43a958f003083a595d759f4c
Abel Schoenmakers <abelschoenmakers@acknowledge.nl>
2025-11-07 12:12:04 +0100

    eerste commit abel

---
+++ abel.py
@@ -0,0 +1,15 @@
+print("Hoi")
+
+a = input("Voer je voornaam in: ")
+print("Gegeven: " + a)
+
+if a == "abel":
+    print("Je bent niet welkom")
+
+
+b = int(input("Wat is je leeftijd: "))
+
+def tweede_functie(a):
+    print("print dit:", a)
+
+tweede_functie(input("Print"))
