20bdafb670a968ca75bab0439bbd68909e321e9f
Esmee van Uden <esmeevanuden@acknowledge.nl>
2025-11-07 12:35:18 +0100

    feature_Esmeetekst

---
+++ esmeedemo.py
@@ -0,0 +1,19 @@
+print("tekst")
+
+
+leeftijd = input("wat is je leeftijd")
+print(leeftijd + 25)
+
+leeftijd = int(leeftijd)
+def ditendat():
+    print("doe dit en dat")
+
+ditendat()
+ditendat()
+ditendat()hj
+
+def tweedefunctie(para):
+    print:("print dit maar", param) 
+
+tweedefunctie(34)
+tweedefunctie(42)
